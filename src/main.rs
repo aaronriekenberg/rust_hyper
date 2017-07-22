4410c970ccd1e13787d798cc2239c83ab826e500
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2017-07-21 21:34:45 -0500

    More playing with hyper.

--- src/main.rs
+++ src/main.rs
@@ -54,49 +54,58 @@ impl Service for Echo {
 
 }
 
-fn serve(thread_id: usize, listen_addr: &SocketAddr, protocol: &Http) {
+fn serve(listen_addr: &SocketAddr, protocol: &Http) {
 
-    info!("starting serve thread_id {}", thread_id);
+  info!("starting {:?}", thread::current().name());
 
-    let mut core = Core::new().expect("error creating core");
-    let handle = core.handle();
+  let mut core = Core::new().expect("error creating core");
+  let handle = core.handle();
 
-    let listener = net2::TcpBuilder::new_v4().expect("error creating listener builder")
-      .reuse_port(true).expect("error setting reuse_port")
-      .bind(listen_addr).expect("error calling bind")
-      .listen(128).expect("error calling listen");
+  let listener = net2::TcpBuilder::new_v4().expect("error creating listener builder")
+    .reuse_port(true).expect("error setting reuse_port")
+    .bind(listen_addr).expect("error calling bind")
+    .listen(128).expect("error calling listen");
 
-    let listener = TcpListener::from_listener(listener, &listen_addr, &handle)
-      .expect("error calling from_listener");
+  let listener = TcpListener::from_listener(listener, &listen_addr, &handle)
+    .expect("error calling from_listener");
 
-    let server =
-      listener.incoming().for_each(|(socket, addr)| {
-        protocol.bind_connection(&handle, socket, addr, Echo);
-        Ok(())
-      });
+  let server =
+    listener.incoming().for_each(|(socket, addr)| {
+      protocol.bind_connection(&handle, socket, addr, Echo);
+      Ok(())
+    });
 
-    core.run(server).expect("error calling core.run");
+  core.run(server).expect("error calling core.run");
 
-    info!("exiting serve thread_id {}", thread_id);
+  info!("exiting {:?}", thread::current().name());
 
 }
 
-fn start_server(num_instances: usize, addr: &str) {
+fn start_server(num_threads: usize, addr: &str) {
 
-    let listen_addr: SocketAddr = addr.parse().unwrap();
+  info!("start_server num_threads = {} addr = {}", num_threads, addr); 
 
-    let protocol = Arc::new(Http::new());
+  let listen_addr: SocketAddr = addr.parse().unwrap();
 
-    for thread_id in 0..(num_instances-1) {
-        let protocol = Arc::clone(&protocol);
-        thread::spawn(move || serve(thread_id, &listen_addr, &protocol));
-    }
+  let protocol = Arc::new(Http::new());
 
-    serve(num_instances-1, &listen_addr, &protocol);
+  let mut thread_handles = Vec::with_capacity(num_threads);
 
+  for i in 0..num_threads {
+    let protocol = Arc::clone(&protocol);
+    thread_handles.push(
+      thread::Builder::new()
+        .name(format!("serve-{}", i))
+        .spawn(move || serve(&listen_addr, &protocol))
+        .expect("spawn error"));
+  }
+
+  while let Some(handle) = thread_handles.pop() {
+    handle.join().expect("join failed");
+  }
 }
 
 fn main() {
-    simple_logger::init_with_level(LogLevel::Info).expect("init_with_level failed");
-    start_server(8, "0.0.0.0:1337");
+  simple_logger::init_with_level(LogLevel::Info).expect("init_with_level failed");
+  start_server(8, "0.0.0.0:1337");
 }
