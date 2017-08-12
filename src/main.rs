2f65c2113e2b7e91299a673ff77aa81ca5ee55e5
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2017-08-12 15:53:14 -0500

    Do not need to depend on futures crate.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate chrono;
 extern crate hyper;
 #[macro_use] extern crate horrorshow;
 extern crate fern;
-extern crate futures;
 extern crate futures_cpupool;
 #[macro_use] extern crate log;
 extern crate mime;
