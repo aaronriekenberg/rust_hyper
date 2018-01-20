23ca54cdd6fe60e221ed2bda69967f3f48b7b181
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2018-01-20 06:41:55 -0600

    Only call expect in main.  Other cleanup.

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ use chrono::prelude::Local;
 use std::sync::Arc;
 
 fn initialize_logging() -> Result<(), fern::InitError>{
+
   fern::Dispatch::new()
     .level(log::LevelFilter::Info)
     .format(|out, message, record| {
@@ -41,19 +42,19 @@ fn initialize_logging() -> Result<(), fern::InitError>{
   Ok(())
 }
 
-fn log_executable_info() {
-  let executable_path = std::env::args().nth(0).expect("missing argument 0");
-  info!("executable_path = {}", executable_path);
+fn log_executable_info(executable_path: String) -> Result<(), std::io::Error> {
 
-  let executable_checksum = utils::file_sha256(executable_path).expect("error getting executable sha256");
+  let executable_checksum = utils::file_sha256(executable_path)?;
   info!("sha256 = {}", executable_checksum);
+
+  Ok(())
 }
 
-fn build_route_configuration(config: &config::Configuration) -> server::RouteConfiguration {
+fn build_route_configuration(config: &config::Configuration) -> Result<server::RouteConfiguration, Box<std::error::Error>> {
+
   let mut path_to_handler = server::RouteConfigurationHandlerMap::new();
 
-  let index_handler =
-    handlers::index::IndexHandler::new(config).expect("error creating IndexHandler");
+  let index_handler = handlers::index::IndexHandler::new(config)?;
   path_to_handler.insert("/".to_string(), Arc::new(index_handler));
 
   for command_info in config.commands() {
@@ -63,7 +64,7 @@ fn build_route_configuration(config: &config::Configuration) -> server::RouteCon
   }
 
   for static_path_info in config.static_paths() {
-    let mime_type = static_path_info.content_type().parse().expect("invalid mime type");
+    let mime_type = static_path_info.content_type().parse()?;
     let handler =
       handlers::static_file::StaticFileHandler::new(
         static_path_info.fs_path().clone(),
@@ -74,15 +75,17 @@ fn build_route_configuration(config: &config::Configuration) -> server::RouteCon
 
   let not_found_handler = handlers::not_found::NotFoundHandler;
 
-  server::RouteConfiguration::new(
-    path_to_handler,
-    Arc::new(not_found_handler))
+  Ok(server::RouteConfiguration::new(
+       path_to_handler,
+       Arc::new(not_found_handler)))
 }
 
 fn main() {
   initialize_logging().expect("failed to initialize logging");
 
-  log_executable_info();
+  let executable_path = std::env::args().nth(0).expect("missing executable command line argument");
+
+  log_executable_info(executable_path).expect("failed to log executable info");
 
   let config_file = std::env::args().nth(1).expect("config file required as command line argument");
 
@@ -91,7 +94,7 @@ fn main() {
 
   let listen_addr = config.listen_address().parse().expect("invalid listen_address");
 
-  let route_configuration = build_route_configuration(&config);
+  let route_configuration = build_route_configuration(&config).expect("failed to build route_configuration");
 
   server::run_forever(
    listen_addr,
